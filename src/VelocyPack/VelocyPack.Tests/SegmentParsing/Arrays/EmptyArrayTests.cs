1ee32fa0f86f6e32de669887ad04df10a7635a2b
Tomas Bosak <bosak.tomas@gmail.com>
2016-11-13 13:59:45 +0100

    Minor clarification of empty array parsing unit test

--- src/VelocyPack/VelocyPack.Tests/SegmentParsing/Arrays/EmptyArrayTests.cs
+++ src/VelocyPack/VelocyPack.Tests/SegmentParsing/Arrays/EmptyArrayTests.cs
@@ -9,14 +9,13 @@ namespace VelocyPack.Tests.SegmentParsing.Arrays
         public void ParseEmptyArray()
         {
             // given
-            // hex dump of [1, 2, 3]
+            // hex dump of []
             var data = new byte[] { 0x01 };
 
             // when
             var segment = VelocyPack.ToSegment(data);
 
             // then
-            // array
             Assert.AreEqual(0, segment.StartIndex);
             Assert.AreEqual(1, segment.CursorIndex);
             Assert.AreEqual(SegmentType.Array, segment.Type);
