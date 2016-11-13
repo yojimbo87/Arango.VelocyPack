be4546dc06bed98f83f7c006af4dc88e2881c521
Tomas Bosak <bosak.tomas@gmail.com>
2016-11-13 13:43:41 +0100

    Add unit test for empty array

---
+++ src/VelocyPack/VelocyPack.Tests/SegmentParsing/Arrays/EmptyArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace VelocyPack.Tests.SegmentParsing.Arrays
+{
+    [TestFixture]
+    public class EmptyArrayTests
+    {
+        [Test]
+        public void ParseEmptyArray()
+        {
+            // given
+            // hex dump of [1, 2, 3]
+            var data = new byte[] { 0x01 };
+
+            // when
+            var segment = VelocyPack.ToSegment(data);
+
+            // then
+            // array
+            Assert.AreEqual(0, segment.StartIndex);
+            Assert.AreEqual(1, segment.CursorIndex);
+            Assert.AreEqual(SegmentType.Array, segment.Type);
+            Assert.AreEqual(ValueType.EmptyArray, segment.ValueType);
+            Assert.AreEqual(0, segment.SubSegments.Count);
+        }
+    }
+}
